439d6aec34f8ab53185135c918bccdd1fda417d6
sagit <sagit@aidock.net>
2020-10-17 13:02:37 +0300

    + power law fix after clarification from yoni + added tests to power law

--- libs/power_law/power_law.py
+++ libs/power_law/power_law.py
@@ -4,7 +4,7 @@ import config
 
 
 class PowerLaw:
-    def __init__(self, chosen_power_law, frequency, L):
+    def __init__(self, chosen_power_law, frequency, polarization, L):
         '''
 
         :param chosen_power_law: a mux which decides what power law is chosen
@@ -14,37 +14,35 @@ class PowerLaw:
         self.L = L
 
         if chosen_power_law == 'Basic':
-            self.db_path =config.basic_db_path
-            self.a, self.b = self.calculate_basic_power_law_constants(frequency)
-
-            self.basic_attinuation_to_rain = self.basic_attinuation_to_rain
-            self.basic_rain_to_attinuation = self.basic_rain_to_attinuation
+            self.db_path = config.basic_db_path
+            self.a, self.b = self.calculate_basic_power_law_constants(frequency,polarization)
         else:
             raise NotImplementedError('power law was not implemented {}'.format(chosen_power_law))
 
-    def calculate_basic_power_law_constants(self, frequency):
+    def calculate_basic_power_law_constants(self, frequency,polarization):
         df = pd.read_csv(self.db_path)
-        row = df[df['frequency[Ghz]']==frequency]
+        row = df[df['frequency[Ghz]'] == frequency]
 
-        if len(row)!=0:
+        if len(row) != 0:
 
-
-
-            # taken from https://www.itu.int/dms_pubrec/itu-r/rec/p/R-REC-P.838-2-200304-S!!PDF-E.pdf
+            # taken from https://www.itu.int/dms_pubrec/itu-r/rec/p/R-REC-P.838-3-200503-I!!PDF-E.pdf
             # k_h k_v alpha_h alpha_v
-
-            theta = 90
-            tau = 45
 
             k_h = row.k_h
             k_v = row.k_v
             a_h = row.a_h
             a_v = row.a_v
 
-            b = (k_h + k_v + (k_h - k_v) * np.cos(theta) ** 2 * np.cos(2 * tau)) / 2
-            a = (k_h * a_v + k_v * a_v + (k_h * a_h - k_v * a_v) * np.cos(theta) ** 2 * np.cos(2 * tau)) / (2 * b)
+            if polarization=='horizontal':
+                return k_h, a_h
 
-            return a, b
+            elif polarization=='vertical':
+                return k_v, a_v
+
+            else:
+                raise ValueError('polarization type is not supported: {}'.format(polarization))
+
+
         else:
             raise ValueError('provided frequency does not exist in table: {}'.format(frequency))
 
@@ -64,5 +62,5 @@ class PowerLaw:
         return self.a * (R) ** self.b * self.L
 
 
-if __name__=="__main__":
-    PowerLaw(chosen_power_law='Basic', frequency=400, L=10)
+if __name__ == "__main__":
+    PowerLaw(chosen_power_law='Basic',frequency=101,polarization='vertical',L=10)
--- libs/power_law/power_law_test.py
+++ libs/power_law/power_law_test.py
@@ -5,18 +5,25 @@ import pandas as pd
 
 
 class PowerLawTest(unittest.TestCase):
+    def setUp(self):
+        self.polarizations = ['horizontal','vertical']
+
 
     def test_create_basic_power_law(self):
-        PowerLaw(chosen_power_law='Basic', frequency=400, L=10)
+        PowerLaw(chosen_power_law='Basic', frequency=400,polarization='vertical', L=10)
+        PowerLaw(chosen_power_law='Basic', frequency=400, polarization='horizontal', L=10)
 
-        with self.assertRaises(ValueError):
-            PowerLaw(chosen_power_law='Basic',frequency=500,L=10)
+        # with self.assertRaises(ValueError):
+        PowerLaw(chosen_power_law='Basic', frequency=400, polarization='cyrcular', L=10)
+        PowerLaw(chosen_power_law='Basic',frequency=101,polarization='vertical',L=10)
 
 
     def test_basic_all_frequecies(self):
         df = pd.read_csv(config.basic_db_path)
-        for frequency in df['frequency[Ghz]']:
-            self.assertTrue(PowerLaw(chosen_power_law='Basic', frequency=frequency, L=10))
+
+        for pol in self.polarizations:
+            for frequency in df['frequency[Ghz]']:
+                self.assertTrue(PowerLaw(chosen_power_law='Basic', frequency=frequency,polarization=pol, L=10))
 
 
 
