06d1d90f058e56d4c6c679c3bacb349594cde85a
sagitiminsky <sagitiminsky@gmail.com>
2020-12-05 16:47:17 +0200

    work on generator

--- libs/dl_models/extractor.py
+++ libs/dl_models/extractor.py
@@ -9,13 +9,13 @@ from datetime import timedelta as dt_delta
 
 class Extractor:
     def __init__(self):
-        self.ims_data,self.ims_max_value, self.ims_min_value = self.load_ims_data()
-        self.dme_data = np.array(self.load_dme_data())
-        self.dme_max_value, self.dme_min_value = np.max(self.dme_data), np.min(self.dme_data)
+        # self.ims_data = self.load_ims_data()
+        self.dme_data = self.load_dme_data()
 
         # norm. - https://datascience.stackexchange.com/questions/5885/how-to-scale-an-array-of-signed-integers-to-range-from-0-to-1
-        self.dme_data = (np.array(self.dme_data) - self.dme_min_value) / (self.dme_max_value - self.dme_min_value)
-        self.ims_data = (np.array(self.ims_data) - self.ims_min_value) / (self.ims_max_value - self.ims_min_value)
+        self.ims_data = (self.ims_data - self.ims_data.min()) / (self.ims_data.max() - self.ims_data.min())
+        self.dme_data = (self.dme_data - self.dme_data.min()) / (self.dme_data.max() - self.dme_data.min())
+
 
         # set dim.
         self.m, self.k_m = self.dme_data.shape
@@ -24,74 +24,59 @@ class Extractor:
         print("X dim: {} - has {} links, {} samples each \nY dim: {} - has {} gauges, {} samples each".format(
             self.dme_data.shape, self.m, self.k_m, self.ims_data.shape, self.n, self.k_n))
 
+    def get_entry(self,arr):
+        i=0
+        while(not('name' in arr[i] and arr[i]['name']=='Rain' )):
+            i+=1
+
+        return arr[i]
+
     def load_ims_data(self):
         if not config.ims_pre_load_data:
 
-            min_ims = 10000
-            max_ims = 0
-
             # 10[min] x 6 is an hour
-            x_test = np.empty((1, 6 * 24 * config.coverage))
+            ims_matrix = np.empty((1, 6 * 24 * config.coverage))
             for index, station_folder in enumerate(os.listdir(config.ims_root_files)):
                 print("now processing gauge: {}".format(station_folder))
                 try:
                     df = pd.read_csv(config.ims_root_files + '/' + station_folder + '/' + 'data.csv')
-                    values = np.empty(1)
+                    ims_vec = np.empty(1)
                     for row_time, row in zip(list(df.datetime), list(df.channels)):
-                        values = np.vstack((values, np.array([ast.literal_eval(row)[0]['value']])))
+                        ims_vec = np.vstack((ims_vec, np.array([self.get_entry(ast.literal_eval(row))['value']])))
 
-                        if max_ims < np.max(values):
-                            max_ims = np.max(values)
-                        if min_ims > np.min(values):
-                            min_ims = np.min(values)
-
-                    values = values[1:].T
+                    ims_vec = ims_vec[1:].T
                     try:
-                        x_test = np.vstack((x_test, values))
+                        ims_matrix = np.vstack((ims_matrix, ims_vec))
                     except ValueError:
                         print("problem with stacking gague {}".format(station_folder))
 
                 except FileNotFoundError:
                     print("data does not exist in {}".format(station_folder))
 
-            x_test = x_test[1:]
+            ims_matrix = ims_matrix[1:]
 
             if not os.path.isdir(config.ims_root_values + '/' + config.date_str_rep):
                 os.makedirs(config.ims_root_values + '/' + config.date_str_rep)
 
             with open(config.ims_root_values + '/' + config.date_str_rep + '/' + 'values.pkl', 'wb') as f:
-                pickle.dump(x_test, f)
-            with open(config.ims_root_values + '/' + config.date_str_rep + '/' + "metadata.txt", 'w') as file:
-                file.write('min: {}\n'.format(min_ims))
-                file.write('max: {}\n'.format(max_ims))
+                pickle.dump(ims_matrix, f)
 
-            return x_test,min_ims,max_ims
+            return ims_matrix
 
         else:
             with open(config.ims_root_values + '/' + config.date_str_rep + '/' + 'values.pkl', 'rb') as f:
-                x_test = pickle.load(f)
-            with open(config.ims_root_values + '/' + config.date_str_rep + '/' + "metadata.txt", 'w') as file:
-                min_ims=float(file.readline().split(' ')[1])
-                max_ims = float(file.readline().split(' ')[1])
+                ims_matrix = pickle.load(f)
 
-            return x_test,min_ims,max_ims
+            return ims_matrix
 
     def load_dme_data(self):
         if not config.dme_pre_load_data:
 
-            """
-            The following dme_matrix is constructed as follows:
-            each element consist of link data, refrence can be found at link_matrix
-            """
-            dme_matrix = []
+
+            # 15[min] x 4 is an hour
+            dme_matrix = np.empty((1, 4 * 24 * config.coverage))
 
             for link in os.listdir(config.dme_root_files):
-
-                """
-                The following link_matrix is constructed as follows:
-                each row consist of 4*96 values the represent a day's worth of mesasurement of a link
-                """
-                link_matrix = []
 
                 link_name = link.split('_')[0]
                 link_type = config.dme_scrape_config['link_objects']['measurement_type']
@@ -103,31 +88,41 @@ class Extractor:
                 time_value = dt.strptime(f"{init_date['yyyy']}-{init_date['mm']}-{init_date['dd']} 00:00:00",
                                          "%Y-%m-%d %H:%M:%S") + dt_delta(days=1)
 
-                dme_vector = []
-                try:
-                    for row_value, row_time, row_interval in zip(list(df['rain']), list(df.Time), list(df.Interval)):
+                dme_vec = np.empty(1)
 
-                        # fill with nan for missing data
-                        while time_value < dt.strptime(row_time, "%Y-%m-%d %H:%M:%S"):
-                            dme_vector.append(np.nan)
-                            time_value = time_value + dt_delta(minutes=15)
-                            if len(dme_vector) == 4 * 24:
-                                link_matrix.append(dme_vector)
-                                dme_vector = []
+                for row_value, row_time, row_interval in zip(list(df['rain']), list(df.Time), list(df.Interval)):
 
-                        if row_interval != 24:
-                            if row_value != np.nan:
-                                dme_vector.append(row_value)
-                            else:
-                                dme_vector.append(np.nan)
-                            time_value = dt.strptime(row_time, "%Y-%m-%d %H:%M:%S") + dt_delta(minutes=15)
-                            if len(dme_vector) == 4 * 24:
-                                link_matrix.append(dme_vector)
-                                dme_vector = []
+                    # fill with nan for missing data
+                    while time_value < dt.strptime(row_time, "%Y-%m-%d %H:%M:%S"):
+                        dme_vec = np.vstack((dme_vec, np.nan))
+                        time_value = time_value + dt_delta(minutes=15)
+                        if dme_vec.size == 4 * 24 * config.coverage:
+                            dme_vec = dme_vec[1:].T
+                            dme_matrix = np.vstack((dme_matrix, dme_vec))
 
-                    dme_matrix.append(link_matrix)
-                except KeyError:
-                    print("now processing link: {} of type: {} was not successful".format(link_name, link_type))
+                    # fill with rain data
+                    if row_interval != 24:
+
+                        if dme_vec.size == 4 * 24 * config.coverage:
+                            dme_vec = dme_vec[1:].T
+                            dme_matrix = np.vstack((dme_matrix, dme_vec))
+
+                        elif row_value != np.nan:
+                            try:
+                                dme_vec = np.vstack((dme_vec, row_value))
+                            except ValueError:
+                                print('error')
+
+
+                        else:
+                            dme_vec = np.vstack((dme_vec, np.nan))
+
+                        time_value = dt.strptime(row_time, "%Y-%m-%d %H:%M:%S") + dt_delta(minutes=15)
+
+
+
+
+            dme_matrix = dme_matrix[1:]
 
             with open(config.dme_root_values + '/' + 'dme_values.pkl', 'wb') as f:
                 pickle.dump(dme_matrix, f)
