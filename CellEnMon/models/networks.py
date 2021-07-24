635757ab2278415434ecdb317f09704feffc17e7
sagitiminsky <sagitiminsky@gmail.com>
2021-07-24 17:18:18 +0300

    create simple descriminator

--- CellEnMon/models/networks.py
+++ CellEnMon/models/networks.py
@@ -200,7 +200,7 @@ def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal'
     norm_layer = get_norm_layer(norm_type=norm)
 
     if netD == 'basic':  # default PatchGAN classifier
-        net = NLayerSequentialDiscriminator(input_nc, ndf, n_layers=3, norm_layer=norm_layer)
+        net = NLayerSequentialDiscriminator(input_nc)
     elif netD == 'n_layers':  # more options
         net = NLayerDiscriminator(input_nc, ndf, n_layers_D, norm_layer=norm_layer)
     elif netD == 'pixel':  # classify if each pixel is real or fake
