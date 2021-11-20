b8a3b0c472039122c81392cb98178c52556a0592
sagitiminsky <sagitiminsky@gmail.com>
2021-11-20 13:38:49 +0200

    pushed successfully to gcs quick fix links without the metadata

--- CellEnMon/libs/scrappers/ims_scrapper/scrapper.py
+++ CellEnMon/libs/scrappers/ims_scrapper/scrapper.py
@@ -4,6 +4,7 @@ import pandas as pd
 import CellEnMon.config as config
 import os
 from datetime import datetime as dt
+from google.cloud import storage
 
 url = "https://api.ims.gov.il/v1/envista/stations/64/data?from=2019/12/01&to=2020/01/01"
 
@@ -11,6 +12,18 @@ headers = {
     'Authorization': 'ApiToken ' + config.ims_token
 }
 
+
+## Setting credentials using the downloaded JSON file
+path = 'cellenmon-e840a9ba53e8.json'
+if not os.path.isfile(path):
+    raise ("Please provide the gcs key in the root directory")
+client = storage.Client.from_service_account_json(json_credentials_path=path)
+
+## For slow upload speed
+storage.blob._DEFAULT_CHUNKSIZE = 2097152  # 1024 * 1024 B * 2 = 2 MB
+storage.blob._MAX_MULTIPART_SIZE = 2097152  # 2 MB
+
+SELECTOR=['UPLOAD'] #'DOWNLOAD', 'UPLOAD'
 
 class IMS_Scrapper_obj:
     def __init__(self, index, station_id, _from, _to):
@@ -21,8 +34,21 @@ class IMS_Scrapper_obj:
         self.root = config.ims_root_files
         self.station_meta_data = f"https://api.ims.gov.il/v1/envista/stations/{station_id}"
         self.station_data = f"https://api.ims.gov.il/v1/envista/stations/{station_id}/data/?from={_from}&to={_to}"
+        self.bucket = client.get_bucket('cell_en_mon')
 
-    def save_metadata(self):
+    def upload_files_to_gcs(self):
+        root=f"datasets/ims/raw/{config.start_date_str_rep}_{config.end_date_str_rep}"
+        for station in os.listdir(root):
+            for file in os.listdir(f'{root}/{station}'):
+                blob = self.bucket.blob(f'ims/{config.start_date_str_rep}-{config.end_date_str_rep}/raw/{station}/{file}')
+                try:
+                    with open(f"{root}/{station}/{file}", 'rb') as f:
+                        blob.upload_from_file(f)
+                    print(f'Uploaded file:{file} succesfully !')
+                except Exception as e:
+                    print(f'Uploaded file:{file} failed with the following exception:{e}!')
+
+    def download_from_ims(self):
         metadata_response = requests.request("GET", self.station_meta_data, headers=headers)
         data_response = requests.request("GET", self.station_data, headers=headers)
 
@@ -61,5 +87,11 @@ if __name__ == "__main__":
 
         print('processing station: {}'.format(index + 1 + config.ims_scrape_config['left_bound']))
 
-        IMS_Scrapper_obj(index=index + 1 + config.ims_scrape_config['left_bound'], station_id=station_id,
-                         _from=config.ims_scrape_config['_from'], _to=config.ims_scrape_config['_to']).save_metadata()
+        obj=IMS_Scrapper_obj(index=index + 1 + config.ims_scrape_config['left_bound'], station_id=station_id,
+                         _from=config.ims_scrape_config['_from'], _to=config.ims_scrape_config['_to'])
+
+        if 'DOWNLOAD' in SELECTOR:
+            obj.download_from_ims()
+
+        if 'UPLOAD' in SELECTOR:
+            obj.upload_files_to_gcs()
