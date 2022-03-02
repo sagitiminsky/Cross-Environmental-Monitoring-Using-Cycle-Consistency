81ea20ba48bcaff3cf37b76ef14fcb86176dd19b
sagit <sagitiminsky@gmail.com>
2022-03-02 11:45:02 +0200

    fix exporter and fix bug for proper datasets for dme

--- CellEnMon/data/exporter.py
+++ CellEnMon/data/exporter.py
@@ -7,10 +7,9 @@ import ast
 
 
 class Domain:
-    def __init__(self, db, metadata_vector_len):
-        self.db = db
-        self.metadat_vector_len = metadata_vector_len
-        self.metadata, self.data = db[:metadata_vector_len], db[metadata_vector_len:]
+    def __init__(self, db):
+        self.station_names_vec=db.keys()
+        self.metadata, self.data = np.array([x['metadata'] for x in db.values()]), np.array([x['data'] for x in db.values()])
         self.data_max, self.data_min, self.data_normalized = self.normalizer(self.data)
         self.metadata_max, self.metadata_min, metadata_normalized = self.normalizer(self.metadata)
 
@@ -23,8 +22,8 @@ class Domain:
 
 class Extractor:
     def __init__(self):
-        self.dme = Domain(*self.load_dme())  # 1 day is 96 = 24*4 data samples + 7 metadata samples
-        self.ims = Domain(*self.load_ims())  # 1 day is 144 = 24*6 data samples + 2 metadata samples
+        self.dme = Domain(self.load_dme())  # 1 day is 96 = 24*4 data samples + 7 metadata samples
+        self.ims = Domain(self.load_ims())  # 1 day is 144 = 24*6 data samples + 2 metadata samples
 
     def stats(self):
         message = f"start:{config.start_date_str_rep} end:{config.end_date_str_rep} --- in total it is {config.coverage} days" \
@@ -52,7 +51,8 @@ class Extractor:
         station_name_splited = station_name.split('-')
         metadata["latitude"] = station_name_splited[3]
         metadata["longitude"] = station_name_splited[4]
-        metadata["vector"] = np.array([metadata['latitude'], metadata['longitude']])
+        metadata["gague_name"]=f"{station_name_splited[0]}-{station_name_splited[1]}-{station_name_splited[2]}"
+        metadata["vector"] = np.array([metadata['longitude'], metadata['latitude']])
         return metadata
 
     def load_ims(self):
@@ -65,7 +65,7 @@ class Extractor:
         except FileNotFoundError:
 
             # 10[min] x 6 is an hour
-            ims_matrix = np.empty((1, 6 * 24 * config.coverage + len(config.ims_metadata)))
+            ims_matrix = {}
             for index, station_file_name in enumerate(os.listdir(f'{config.ims_root_files}/raw')):
                 print("now processing gauge: {}".format(station_file_name))
                 try:
@@ -81,13 +81,18 @@ class Extractor:
                         ims_vec = ims_vec.T
                         try:
                             ims_matrix = np.vstack((ims_matrix, ims_vec))
+
+                            ims_matrix[metadata["gauge_name"]] = \
+                                {
+                                    "data": ims_matrix[metadata["gauge_name"]] + ims_vec
+                                    "metadata": metadata["vector"]
+                                }
+
                         except ValueError:
                             print("problem with stacking gague {}".format(station_file_name))
 
                 except FileNotFoundError:
                     print("data does not exist in {}".format(station_file_name))
-
-            ims_matrix = ims_matrix[1:]
 
             if not os.path.isdir(temp_str):
                 os.makedirs(temp_str)
@@ -95,7 +100,7 @@ class Extractor:
             with open(f'{temp_str}/values.pkl', 'wb') as f:
                 pickle.dump(ims_matrix, f)
 
-        return ims_matrix.astype(np.float), len(metadata["vector"])
+        return ims_matrix
 
     def get_dme_metadata(self, link_file_name):
         metadata = {}
@@ -122,34 +127,34 @@ class Extractor:
 
             # 15[min] x 4 is an hour
             valid_row_number = 4 * 24 * config.coverage
-            dme_matrix = np.empty((1, valid_row_number + len(config.dme_metadata)))
+            dme_matrix = {}
 
             for link_file_name in os.listdir(f'{config.dme_root_files}/raw'):
 
-                link_metadata = self.get_dme_metadata(link_file_name)
+                metadata = self.get_dme_metadata(link_file_name)
                 link_type = config.dme_scrape_config['link_objects']['measurement_type']
 
                 print(
-                    "preprocessing: now processing link: {} of type: {}".format(link_metadata["link_name"], link_type))
+                    "preprocessing: now processing link: {} of type: {}".format(metadata["link_name"], link_type))
 
                 df = pd.read_csv(f"{config.dme_root_files}/raw/{link_file_name}")
                 df = df[df.Interval == '15']
 
                 if len(list(df['Time'])) != valid_row_number:
                     print(
-                        f'Number of rows for link: {link_metadata["link_name"]} is wrong: {len(list(df["Time"]))}!={valid_row_number}')
+                        f'Number of rows for link: {metadata["link_name"]} is wrong: {len(list(df["Time"]))}!={valid_row_number}')
 
                 else:
-                    if 'RFInputPower' in df:
-                        stack = [link_metadata["vector"] + x for x in [df.RFInputPower, df.RFOutputPower]]
-                    elif 'PowerRLTMmin' in df:
-                        stack = [link_metadata["vector"] + x for x in
-                                 [df.PowerTLTMmax, df.PowerTLTMmin, df.PowerRLTMmax, df.PowerRLTMmin]]
+                    # if 'RFInputPower' in df:
+                    #     stack = [link_metadata["vector"] + x for x in [df.RFInputPower, df.RFOutputPower]]
+                    if 'PowerTLTMmax' in df and 'PowerTLTMmin' in df and 'PowerRLTMmax' in df and 'PowerRLTMmax' in df:
+                        dme_matrix[metadata["link_name"]] = \
+                            {
+                                "data": dme_matrix[metadata["link_name"]] + [df.PowerTLTMmax, df.PowerTLTMmin, df.PowerRLTMmax, df.PowerRLTMmin]
+                                "metadata": metadata["vector"]
+                            }
                     else:
-                        raise Exception("Unsupported field in link")
-                    dme_matrix = np.vstack(dme_matrix, stack)
-
-            dme_matrix = dme_matrix[1:]
+                        print(f"Not all fields were provided in link:{metadata['link_name']}")
 
             if not os.path.isdir(temp_str):
                 os.makedirs(temp_str)
@@ -157,7 +162,7 @@ class Extractor:
             with open(f'{temp_str}/values.pkl', 'wb') as f:
                 pickle.dump(dme_matrix, f)
 
-        return dme_matrix.astype(np.float), len(link_metadata["vector"])
+        return dme_matrix
 
 
 if __name__ == "__main__":
