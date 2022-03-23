f5dc06191e4f12cc579b3532dd47ab5e94164c82
sagit <sagitiminsky@gmail.com>
2022-03-23 15:43:41 +0200

    work on secret managment for aws secrets pull work

--- CellEnMon/dataset_manager.sh
+++ CellEnMon/dataset_manager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-PROVIDER=$1 # aws | gcp
+MODE=upload # upload | download
+PROVIDER=$1 # s3 | gs
 DB=$2 # dme | ims
 START_DATE=$3 #ddmmyyyy
 END_DATE=$4 #ddmmyyyy
@@ -8,17 +8,33 @@ TYPE=$5 # raw | processed
 
 echo "print args $@"
 
-if [ $PROVIDER == "gcp" ]; then
+remote="$PROVIDER"'://cell_en_mon/'"$DB"'/'"$START_DATE"'-'"$END_DATE"'/'"raw"'/'
+local='./CellEnMon/datasets/'"$DB"'/'"$TYPE"'/'"$START_DATE"'-'"$END_DATE"
 
+if [ $PROVIDER == "gcp" ]; then
+  #google auth
   gcloud config set project cellenmon
-  source='gs://cell_en_mon/'"$DB"'/'"$START_DATE"'-'"$END_DATE"'/'"raw"'/'
-  dest='./CellEnMon/datasets/'"$DB"'/'"$TYPE"'/'"$START_DATE"'-'"$END_DATE"
-  mkdir -p $dest
-  gsutil -m cp -r  $source $dest
+
+  if [ $MODE == "download"]; then
+    mkdir -p $dest
+    gsutil -m cp -r  $remote $local
+  else
+    gsutil -m cp -r  $local $remote
+  fi
 
 elif [ $PROVIDEDR == "aws" ]; then
-  echo "$AWS_config"  | base64 -d > ~/.aws/config
-  echo "$AWS_credentials" | base64 -d > ~/.aws/credentials
+  #aws auth
+  export AWS_ACCESS_KEY_ID=python3 CellEnMon/libs/vault/vault.py aws key
+  export AWS_SECRET_ACCESS_KEY=python3 CellEnMon/libs/vault/vault.py aws secret
+
+    if [ $MODE == "download"]; then
+    mkdir -p $dest
+    s3 -m cp -r  $remote $local
+  else
+    s3 -m cp -r  $local $remote
+  fi
+
+
 
 else
 fi
