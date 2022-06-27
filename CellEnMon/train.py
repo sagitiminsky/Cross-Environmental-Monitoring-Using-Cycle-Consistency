05771f2880391451077477f709b53df10ee924e0
sagit <sagitiminsky@gmail.com>
2022-06-27 14:02:03 +0300

    first commit

--- CellEnMon/train.py
+++ CellEnMon/train.py
@@ -1,40 +1,50 @@
 import time
 from options.train_options import TrainOptions
+from options.test_options import TestOptions
 import data
 import models
 import wandb
-ENABLE_WANDB=False
+ENABLE_WANDB=True
 
 
 if __name__ == '__main__':
-    opt = TrainOptions().parse()  # get training options
+    train_opt = TrainOptions().parse()  # get training options
+    validation_opt = TestOptions().parse()
     if ENABLE_WANDB:
-        wandb.init(project=opt.name,  entity='sagitiminsky')
-    dataset = data.create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    model = models.create_model(opt)  # create a model given opt.model and other options
-    model.setup(opt)  # regular setup: load and print networks; create schedulers
+        wandb.init(project=train_opt.name,  entity='sagitiminsky')
+    train_dataset = data.create_dataset(train_opt)  # create a train dataset given opt.dataset_mode and other options
+    validation_dataset = data.create_dataset(train_opt)
+    model = models.create_model(train_opt)  # create a model given opt.model and other options
+    model.setup(train_opt)  # regular setup: load and print networks; create schedulers
     total_iters = 0  # the total number of training iterations
 
-    for epoch in range(opt.epoch_count,
-                       opt.n_epochs + opt.n_epochs_decay + 1):  # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
+    for epoch in range(train_opt.epoch_count,
+                       train_opt.n_epochs + train_opt.n_epochs_decay + 1):  # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()  # timer for data loading per iteration
         epoch_iter = 0  # the number of training iterations in current epoch, reset to 0 every epoch
         model.update_learning_rate()  # update learning rates in the beginning of every epoch.
-        for i, data in enumerate(dataset):  # inner loop within one epoch
+        for i, data in enumerate(train_dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
-            if total_iters % opt.print_freq == 0:
+            if total_iters % train_opt.print_freq == 0:
                 t_data = iter_start_time - iter_data_time
 
-            total_iters += opt.batch_size
-            epoch_iter += opt.batch_size
+            total_iters += train_opt.batch_size
+            epoch_iter += train_opt.batch_size
             #TODO: on setinput we currently take only the data, we should consider using the metadata too
             model.set_input(data)  # unpack data from dataset and apply preprocessing
             model.optimize_parameters()  # calculate loss functions, get gradients, update network weights
 
-            if total_iters % opt.print_freq == 0:  # print training losses and save logging information to the disk
+            if total_iters % train_opt.print_freq == 0:  # print training and validation losses and save logging information to the disk
+                # Training losses
                 losses = model.get_current_losses()
-                t_comp = (time.time() - iter_start_time) / opt.batch_size
+                t_comp = (time.time() - iter_start_time) / train_opt.batch_size
+
+                for i,val_data in enumerate(validation_dataset):
+                    model.set_input(data)
+                    model.test()
+
+
                 print(f"losses:{losses}")
                 if ENABLE_WANDB:
                     wandb.log(losses)
