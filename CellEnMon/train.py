615b36d6cd6a7dfbc405a03874efb9dd7ab31f6a
sagit <sagitiminsky@gmail.com>
2022-07-20 18:36:55 +0300

    display the metadata on the plots

--- CellEnMon/train.py
+++ CellEnMon/train.py
@@ -108,43 +108,40 @@ if __name__ == '__main__':
                 plt.clf()
 
                 visuals = model.get_current_visuals()
-                if train_opt.is_only_dynamic:
-                    fig, axs = plt.subplots(2, 4, figsize=(15, 15))
-                    plt.title(f'{model.link}<->{model.gague}')
-                    for ax, key in zip(axs.flatten(), visuals):
-                        display = [[mpl_dates.date2num(datetime.strptime(t[0], '%Y-%m-%d %H:%M:%S'))] + data.tolist()
-                                   for t, data in
-                                   zip(model.t, visuals[key][0][0][:,:4].cpu().detach().numpy())]
+                fig, axs = plt.subplots(2, 4, figsize=(15, 15))
+                plt.title(f'{model.link}<->{model.gague}')
+                for ax, key in zip(axs.flatten(), visuals):
+                    N=4 if train_opt.is_only_dynamic else 8
+                    display = [[mpl_dates.date2num(datetime.strptime(t[0], '%Y-%m-%d %H:%M:%S'))] + data.tolist()
+                               for t, data in
+                               zip(model.t, visuals[key][0][0][:,:N].cpu().detach().numpy())]
 
-                        # Plot Data
-                        for i in range(1, 5):
-                            if 'A' in key:
-                                mmin=model.data_transformation['link']['min'][0].numpy()
-                                mmax=model.data_transformation['link']['max'][0].numpy()
-                                label=DME_KEYS[i]
-                            else:
-                                mmin = model.data_transformation['gague']['min'][0].numpy()
-                                mmax = model.data_transformation['gague']['max'][0].numpy()
-                                label=IMS_KEYS[i]
+                    # Plot Data
+                    for i in range(1, 5):
+                        if 'A' in key:
+                            mmin=model.data_transformation['link']['min'][0].numpy()
+                            mmax=model.data_transformation['link']['max'][0].numpy()
+                            label=DME_KEYS[i]
+                        else:
+                            mmin = model.data_transformation['gague']['min'][0].numpy()
+                            mmax = model.data_transformation['gague']['max'][0].numpy()
+                            label=IMS_KEYS[i]
 
-                            data = visuals[key][0][0][:,i-1].cpu().detach().numpy()
-                            ax.plot([mpl_dates.date2num(datetime.strptime(t[0], '%Y-%m-%d %H:%M:%S')) for t in model.t],
-                                    min_max_inv_transform(data, mmin=mmin, mmax=mmax),
-                                    marker='o',
-                                    linestyle='dashed',
-                                    linewidth=0.0,
-                                    markersize=4,
-                                    label=label
-                                    )
-                            ax.set_title(key, y=0.75)
+                        data = visuals[key][0][0][:,i-1].cpu().detach().numpy()
+                        ax.plot([mpl_dates.date2num(datetime.strptime(t[0], '%Y-%m-%d %H:%M:%S')) for t in model.t],
+                                min_max_inv_transform(data, mmin=mmin, mmax=mmax),
+                                marker='o',
+                                linestyle='dashed',
+                                linewidth=0.0,
+                                markersize=4,
+                                label=label
+                                )
+                        ax.set_title(key if train_opt.is_only_dynamic else f'{key} \n {visuals[key][0][0][0,4:8]}', y=0.75)
 
-                        # Formatting Date
-                        date_format = mpl_dates.DateFormatter('%Y-%m-%d %H:%M:%S')
-                        ax.xaxis.set_major_formatter(date_format)
+                    # Formatting Date
+                    date_format = mpl_dates.DateFormatter('%Y-%m-%d %H:%M:%S')
+                    ax.xaxis.set_major_formatter(date_format)
 
-
-                else:
-                    raise NotImplementedError
 
                 wandb.log({**validation_losses, **training_losses})
                 wandb.log({"Images": [wandb.Image(visuals[key], caption=key) for key in visuals]})
