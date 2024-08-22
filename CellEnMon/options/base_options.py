272c8bd7772d25e8b396e8a3b3df8e37cb5dcc24
EC2 Default User <ec2-user@ip-172-16-38-136.ec2.internal>
2024-08-22 16:09:57 +0000

    breakthrough with norm type batch

--- CellEnMon/options/base_options.py
+++ CellEnMon/options/base_options.py
@@ -34,19 +34,19 @@ class BaseOptions():
         parser.add_argument('--output_nc_A', type=int, default=2, help='# of output channels') # in the output we want to duplicate the 64 vector | one for regression the other for classification, so 4x64->2x64
         parser.add_argument('--input_nc_B', type=int, default=1, help='# of input channels')
         parser.add_argument('--output_nc_B', type=int, default=4, help='# of output channels') # here we go from Rain to Att, so 1x64->4x64
-        parser.add_argument('--ngf', type=int, default=128, help='[1,2] ONLY |# of gen filters in the last conv layer')
+        parser.add_argument('--ngf', type=int, default=2, help='[1,2] ONLY |# of gen filters in the last conv layer')
         parser.add_argument('--ndf', type=int, default=4, help='# of discrim filters in the first conv layer')
         parser.add_argument('--netD', type=str, default='n_layers',
                             help='specify discriminator architecture [n_layers]. n_layers allows you to specify the layers in the discriminator')
         parser.add_argument('--netG', type=str, default='resnet_9blocks', help='specify generator architecture [n_layers]')
         parser.add_argument('--n_layers_D', type=int, default=1, help='only used if netD==n_layers')
-        parser.add_argument('--norm', type=str, default='instance',
-                            help='instance normalization or batch normalization [instance | batch | none]')
+        parser.add_argument('--norm', type=str, default='batch',
+                            help='instance normalization or batch normalization [instance | batch | none | layer]')
         parser.add_argument('--init_type', type=str, default='kaiming',
                             help='network initialization [normal | xavier | kaiming | orthogonal]')
         parser.add_argument('--init_gain', type=float, default=0.02,
                             help='scaling factor for normal, xavier and orthogonal.')
-        parser.add_argument('--no_dropout', action='store_true', default=True, help='no dropout for the generator')
+        parser.add_argument('--no_dropout', action='store_true', default=False, help='no dropout for the generator')
         # dataset parameters
         parser.add_argument('--dataset_mode', type=str, default='cellenmon', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
         parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
