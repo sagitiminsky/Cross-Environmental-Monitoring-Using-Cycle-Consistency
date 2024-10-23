0379afe98ef19390fa3bed86474eacd0f473a098
Sagi Timinsky <sagitiminsky@gmail.com>
2024-10-23 07:28:37 +0000

    reversed back

--- CellEnMon/train.py
+++ CellEnMon/train.py
@@ -92,6 +92,7 @@ validation_link_to_gauge_matching ={
 # Threshold for binary classification
 threshold = float(os.environ["threshold"])
 probability_threshold = float(os.environ["probability_threshold"]) #0.3 # a*e^(-bx)+c, ie. we consider a wet event over x=0.2 mm/h
+ITERS_BETWEEN_VALIDATIONS=10000
 
 # Detection:
 #[[  52 2099]
@@ -200,7 +201,7 @@ if __name__ == '__main__':
             
             
             
-        if epoch % 1000 == 0 and epoch>0: # VALIDATION
+        if epoch % ITERS_BETWEEN_VALIDATIONS == 0 and epoch>0: # VALIDATION
             
 
             print(f"Validation in progress... Current lr is: {model.optimizers[0].param_groups[0]['lr']}")
@@ -555,7 +556,7 @@ if __name__ == '__main__':
             if ENABLE_WANDB:
 #                 wandb.log({"Real vs Fake": rain_fig})
                 for key in current_losses:
-                    training_losses[key] = training_losses[key]/(1000*len(train_dataset))
+                    training_losses[key] = training_losses[key]/(ITERS_BETWEEN_VALIDATIONS * len(train_dataset))
             
                 wandb.log({**training_losses})
                 path_to_html = f"{v.out_path}/{v.map_name}"
