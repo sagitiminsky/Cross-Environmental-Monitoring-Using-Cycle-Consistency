426871962df9da743a213a17668b6475cee7f74a
Sagi Timinsky <sagitiminsky@gmail.com>
2024-11-09 16:55:08 +0000

    save point

--- CellEnMon/options/base_options.py
+++ CellEnMon/options/base_options.py
@@ -40,7 +40,7 @@ class BaseOptions():
                             help='specify discriminator architecture [n_layers]. n_layers allows you to specify the layers in the discriminator')
         
         
-        parser.add_argument('--netG', type=str, default='resnet_1blocks', help='specify generator architecture [n_layers]')
+        parser.add_argument('--netG', type=str, default='resnet_6blocks', help='specify generator architecture [n_layers]')
         parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers | if you want to change this make sure to adjust D so it will ouput [1,1,1]')
         parser.add_argument('--norm', type=str, default='batch',
                             help='instance normalization or batch normalization [instance | batch | none | layer]')
