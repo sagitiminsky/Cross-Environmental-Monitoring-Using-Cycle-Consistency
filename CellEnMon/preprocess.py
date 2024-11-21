6ae9bcb0060535e4e5710269430595fc16a4a140
Sagi Timinsky <sagitiminsky@gmail.com>
2024-11-21 14:10:19 +0000

    best results so far

--- CellEnMon/preprocess.py
+++ CellEnMon/preprocess.py
@@ -22,8 +22,8 @@ class Preprocess:
         rec_detections = np.asarray(df_detections['rec_detections'],dtype=float)
         
         
-         #
-         #
+         #* fake_detections
+         #* rec_detections
 
         d = {'Time':pd.to_datetime(T),\
                 'real':real,\
@@ -31,8 +31,8 @@ class Preprocess:
                 'rec': rec,\
                 'fake_det': fake_detections,\
                 'rec_det': rec_detections,\
-                "fake_dot_det": fake * fake_detections,\
-                "rec_dot_det": rec * rec_detections
+                "fake_dot_det": fake ,\
+                "rec_dot_det": rec 
                 
             }
         df = pd.DataFrame(data=d)
